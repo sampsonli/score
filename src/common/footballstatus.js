1feea95626d257a06227b5c59882bb3f596b33a8
sampsonli <740056710@qq.com>
2017-05-31 18:00:25 +0800

    modify

---
+++ src/common/footballstatus.js
@@ -0,0 +1,30 @@
+    /**
+     * 足球比赛的状态
+     *
+     * "0": "未开始",
+     * "1": "上半场",
+     * "2": "中场",
+     * "3": "下半场",
+     * "4": "完场",
+     * "5": "取消",
+     * "6": "改期",
+     * "7": "腰斩",
+     * "8": "中断",
+     * "9": "待定",
+     */
+    export default {
+        NOT_STARTED: 0,
+        FIRST_HALF: 1,
+        MID: 2,
+        LAST_HALF: 3,
+        ENDED: 4,
+        CANCELED: 5,
+        CHANGED: 6,
+        REMOVED: 7,
+        PAUSED: 8,
+        UNSURE: 9,
+        EXTRA_STARTED: 10,
+        EXTRA_ENDED: 11,
+        SPOT_KICK_STARTED: 12,
+        SPOT_KICK_ENDED: 13
+    }
