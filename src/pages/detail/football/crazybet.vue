04daf39e55b333dd5116de1759173d57c830e072
sampsonli <740056710@qq.com>
2017-06-03 18:23:39 +0800

    fix bug

--- src/pages/detail/football/crazybet.vue
+++ src/pages/detail/football/crazybet.vue
@@ -1,15 +1,20 @@
 <template>
-    <div id="crazybetframe" style="overflow: hidden; height: 0;">
-        <iframe id="_crazybet_frame" style="padding: 0;margin: 0;height: 100%;width: 100%;border: 0" src="http://crazybet.choopaoo.com/500bf/crazy/index.html#/h5/home/648289"></iframe>
+    <div id="crazybetframe" style="overflow: hidden; height: 2000px;">
+        <iframe id="_crazybet_frame" style="padding: 0;margin: 0;height: 0;width: 100%;border: 0"></iframe>
     </div>
 </template>
 <script>
     export default {
         mounted () {
-            let wrapper = document.querySelector('._scroll_container')
+            let detailTop = document.querySelector('.detailTop')
             let navigator = document.querySelector('.navigator')
-            let crazybetframe = document.querySelector('#crazybetframe')
-            crazybetframe.style.height = (wrapper.offsetHeight - navigator.offsetHeight) + 'px'
+            let crazybetframe = document.querySelector('#_crazybet_frame')
+            crazybetframe.style.height = (innerHeight - detailTop.offsetHeight - navigator.offsetHeight) + 'px'
+            crazybetframe.src = `http://crazybet.choopaoo.com/500bf/crazy/index.html#/h5/home/${this.$route.params.fid}`
+        },
+        methods: {
+            raf: (cb) => window.requestAnimationFrame ? requestAnimationFrame(cb) : setTimeout(() => cb(), 16.7)
+
         }
     }
 
