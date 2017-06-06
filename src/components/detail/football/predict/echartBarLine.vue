7e7ba6116aa763feb9809797e042c99c3636acf2
sampsonli <740056710@qq.com>
2017-06-06 17:53:20 +0800

    fixbug

--- src/components/detail/football/predict/echartBarLine.vue
+++ src/components/detail/football/predict/echartBarLine.vue
@@ -6,6 +6,8 @@
     import echarts from 'echarts/lib/echarts'
     import 'echarts/lib/chart/line'
     import 'echarts/lib/chart/bar'
+    import 'echarts/lib/component/legend'
+
     export default {
         props: {
             nameBar: {
